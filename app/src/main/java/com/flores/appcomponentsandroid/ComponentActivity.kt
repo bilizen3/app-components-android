0976f0046dd5da631487831815b934ba7ea8debd
Bill Flores <billflores@Bills-iMac.local>
2019-05-07 11:47:28 -0500

    add view for activity component

---
+++ app/src/main/java/com/flores/appcomponentsandroid/ComponentActivity.kt
@@ -0,0 +1,12 @@
+package com.flores.appcomponentsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComponentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_component)
+    }
+}
