ecb86fa9628177b02b3c6921a50e083721d9a54d
Bill Flores <billflores@Bills-iMac.local>
2019-05-07 19:02:04 -0500

    add services

--- app/src/main/java/com/flores/appcomponentsandroid/service/MyService.kt
+++ app/src/main/java/com/flores/appcomponentsandroid/service/MyService.kt
@@ -3,22 +3,33 @@ package com.flores.appcomponentsandroid.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.media.MediaPlayer
+import android.provider.Settings
+
 
 class MyService : Service() {
+
+    private var player: MediaPlayer? = null
+
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
-    }
+        player = MediaPlayer.create(
+            this,
+            Settings.System.DEFAULT_RINGTONE_URI
+        )
+        player?.isLooping = true
+        player?.start()
 
+        return START_STICKY
+    }
 
     override fun onDestroy() {
         super.onDestroy()
-
+        player?.stop()
     }
-
 
 }
--- app/src/main/java/com/flores/appcomponentsandroid/service/ServicesActivity.kt
+++ app/src/main/java/com/flores/appcomponentsandroid/service/ServicesActivity.kt
@@ -1,14 +1,23 @@
 package com.flores.appcomponentsandroid.service
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flores.appcomponentsandroid.R
+import kotlinx.android.synthetic.main.activity_services.*
 
 class ServicesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_services)
-    }
 
+        btnStartService.setOnClickListener {
+            startService(Intent(this, MyService::class.java))
+        }
+
+        btnStopService.setOnClickListener {
+            stopService(Intent(this, MyService::class.java))
+        }
+    }
 }
